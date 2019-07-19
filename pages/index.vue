a41681e6c69bae1e7297f5ff70a806145a9e2aca
GregJacobs82 <greg@2book.com>
2019-07-19 16:10:41 -0400

    Setup Bootstrap-vue navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,70 @@
 <template>
-  <div class="container">
-    <nuxt-link to="/users">Users</nuxt-link>
+  <div class="index-template">
     <div>
-      <logo />
-      <h1 class="title">
-        Testing 123
-      </h1>
-      <h2 class="subtitle">
-        Api Calls using faker json data
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">Faker_API</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item href="#">
+                    <nuxt-link to="/users">Users</nuxt-link>
+                </b-nav-item>
+                <b-nav-item href="#" disabled>Disabled</b-nav-item>
+            </b-navbar-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+                <b-nav-form>
+                <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                </b-nav-form>
+
+                <b-nav-item-dropdown text="Lang" right>
+                <b-dropdown-item href="#">EN</b-dropdown-item>
+                <b-dropdown-item href="#">ES</b-dropdown-item>
+                <b-dropdown-item href="#">RU</b-dropdown-item>
+                <b-dropdown-item href="#">FA</b-dropdown-item>
+                </b-nav-item-dropdown>
+
+                <b-nav-item-dropdown right>
+                <!-- Using 'button-content' slot -->
+                <template slot="button-content"><em>User</em></template>
+                <b-dropdown-item href="#">Profile</b-dropdown-item>
+                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                </b-nav-item-dropdown>
+            </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+    <div class="nuxt-container">
+        <div>
+            <logo />
+            <h1 class="title">Faker API</h1>
+            <h2 class="subtitle">Api Calls using faker json data</h2>
+            <div class="links">
+                <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+                <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+            </div>
+        </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue";
+import { Navbar } from 'bootstrap-vue';
 
 export default {
   components: {
     Logo
   }
-}
+};
 </script>
 
 <style>
-.container {
+.nuxt-container {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
@@ -50,8 +74,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
